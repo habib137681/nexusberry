05ace71d2dc59c3f1ac877f63678dfef950478ca
habib137681 <you@example.com>
2021-11-07 14:10:46 +0500

    App is updated with carousel

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 function App() {
   return (
     <>
-    <h1>Get this code to github repository</h1>
+    <h1>Get this code to github repository 1</h1>
+    <h1>Get this code to github repository 2</h1>
     </>
   );
 }
